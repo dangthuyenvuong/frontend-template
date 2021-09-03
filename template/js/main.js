0354a47b3315bcbcb9028d2428025879e9be58b1
Vuong Dang <vuongdang@chidoanh.com>
2021-09-03 21:56:55 +0700

    update

--- template/js/main.js
+++ template/js/main.js
@@ -14,11 +14,11 @@
 
         let errorObject = {}
 
-        if(!data.email){
+        if (!data.email) {
             errorObject.email = 'Vui lòng điền Email'
         }
 
-        for(let i in errorObject){
+        for (let i in errorObject) {
             let errorNode = document.createElement('span')
             errorNode.classList.add('error-text')
             errorNode.innerHTML = errorObject[i]
@@ -27,7 +27,7 @@
         }
 
 
-        if(Object.keys(errorObject).length > 0){
+        if (Object.keys(errorObject).length > 0) {
             e.preventDefault()
         }
 
@@ -35,11 +35,17 @@
 
 
 })()
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+
 
 
 let $header = document.querySelector('#header')
 let $banner = document.querySelector('.banner')
-let $menus = document.querySelectorAll('#header nav a') 
+let $menus = document.querySelectorAll('#header nav a')
 
 let scrollTopCondition = $banner.offsetHeight - $header.offsetHeight
 
@@ -48,47 +54,43 @@ let $activeMenuBrder = document.querySelector('#header .active-menu')
 let $anchors = []
 
 $menus.forEach(e => {
-    $anchors.push( document.querySelector(e.getAttribute('href')) )
+    $anchors.push(document.querySelector(e.getAttribute('href')))
 })
 
-window.addEventListener('scroll', function(){
-    let scrollTop = document.documentElement.scrollTop
-    
-    if(scrollTop > scrollTopCondition){
+scroller.on('scroll', function (position) {
+    let scrollTop = position.scroll.y
+
+    if (scrollTop > scrollTopCondition) {
         $header.classList.add('fixed')
-    }else{
+    } else {
         $header.classList.remove('fixed')
     }
 
 
     $anchors.forEach(e => {
-        if(scrollTop >= e.offsetTop){
+        if (scrollTop >= e.offsetTop) {
             let menu = document.querySelector(`#header [href="#${e.id}"]`)
             $activeMenuBrder.style.width = `${menu.offsetWidth - 30}px`
             $activeMenuBrder.style.left = `${menu.parentNode.offsetLeft + 15}px`
-        }   
+        }
     })
 
-    
-})
 
+})
 // document.body.addEventListener('click', function() {
 //     console.log('body click')
 // })
 
 $menus.forEach(e => {
-    e.addEventListener('click', function(event){
+    e.addEventListener('click', function (event) {
         event.preventDefault()
         // event.stopPropagation()
 
         let id = this.getAttribute('href')
-        
+
         let anchor = document.querySelector(id)
 
-        window.scrollTo({
-            top: anchor.offsetTop,
-            behavior: 'smooth'
-        });
+        scroller.scrollTo(anchor.offsetTop);
 
     })
 })
@@ -108,23 +110,23 @@ $button.addEventListener('click', (e) => {
         data[e.name] = e.value
     })
 
-    
+
     let errorObject = {}
 
-    if(!data.name){
+    if (!data.name) {
         errorObject.name = 'Vui lòng điền họ và tên'
     }
-    if(!data.email){
+    if (!data.email) {
         errorObject.email = 'Vui lòng điền Email'
     }
-    if(!data.subject){
+    if (!data.subject) {
         errorObject.subject = 'Vui lòng điền Subject'
     }
-    if(!data.content){
+    if (!data.content) {
         errorObject.content = 'Vui lòng điền Nội dung'
     }
 
-    for(let i in errorObject){
+    for (let i in errorObject) {
         let errorNode = document.createElement('p')
         errorNode.classList.add('erorr-text')
         errorNode.innerHTML = errorObject[i]
@@ -133,10 +135,36 @@ $button.addEventListener('click', (e) => {
     }
 
 
-    if(Object.keys(errorObject).length > 0){
+    if (Object.keys(errorObject).length > 0) {
         e.preventDefault()
     }
 
 })
+
+
+
+let section6CoverImgs = document.querySelectorAll('.section6 .post .cover img')
+
+
+let elem = document.querySelector('.main-carousel');
+let flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: true,
+    wrapAround: true,
+    on: {
+        change(index) {
+            document.querySelector('.section6 .post .cover img.active').classList.remove('active')
+            section6CoverImgs[index].classList.add('active')
+        }
+    }
+});
+
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity( '.main-carousel', {
+//   // options
+// });
 
 
